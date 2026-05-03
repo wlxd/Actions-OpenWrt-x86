680b25539b1ffc3d50638a7f32bc952221485934
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2026-05-03 18:45:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,12 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# 1. 替换 Luci 的默认核心主题依赖
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+# 2. 移除 Lean 源码中硬编码对 Argon 的默认指向
+sed -i 's/luci-theme-argon/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+# 3. 强行修改底层默认设置脚本中的初始开机 UI 路径
+sed -i 's/luci-static\/argon/luci-static\/design/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/x86/Makefile
 #sed -i "s/.*PKG_VERSION:=.*/PKG_VERSION:=4.3.9_v1.2.14/" package/lean/qBittorrent-static/Makefile
 #sed -i "s/.*PKG_VERSION:=.*/PKG_VERSION:=5.0.0-stable/" package/libs/wolfssl/Makefile
